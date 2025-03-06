3bba12c0c0e24d2655d142f44449f8a966cb53d6
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2025-03-06 15:10:23 +0300

    initialize of the ui

---
+++ ecomerce_ui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>My Ecommerce</h1>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
