a10d93c7ecc28f37aadfd420f27cf2a9da4b58bb
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2025-03-17 22:54:57 +0300

    Activate a product

--- API/controllers/productsController.js
+++ API/controllers/productsController.js
@@ -78,6 +78,7 @@ function deactivateProduct(req, res) {
                 console.log("Error occured in query", err);
                 return;
             };
+
             if (result.rowsAffected[0] === 0) {
                 return res.status(404).json({
                 success: false,
@@ -94,9 +95,43 @@ function deactivateProduct(req, res) {
     );
 };
 
+function activateProduct (req, res){
+    let pool = req.pool;
+    let requestedProductId = req.params.productId;
+
+    pool.query(
+        `UPDATE products
+        SET ProductStatus = 'Active'
+        WHERE ProductId = ${requestedProductId}`, (err, result) =>{
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    message: "Internal server error."
+                });
+                console.log("Error occured in query", err);
+                return;
+            };
+
+            if (result.rowsAffected[0] === 0) {
+                return res.status(404).json({
+                success: false,
+                message: `Product with ID ${requestedProductId} not found.`,
+                });
+            } else {
+                res.json({
+                success: true,
+                message: "Product was successfully activated.",
+                rowsAffected: result.rowsAffected,
+                });
+            }
+        }
+    )
+}
+
 module.exports = {
     getAllProducts,
     getMostSellingProduct,
     getLowQuantityProducts,
-    deactivateProduct
+    deactivateProduct,
+    activateProduct
 };
--- API/routes/productsRoutes.js
+++ API/routes/productsRoutes.js
@@ -4,10 +4,12 @@ const {
     getMostSellingProduct,
     getLowQuantityProducts,
     deactivateProduct,
+    activateProduct,
 } = require("../controllers/productsController");
 
 productsRouter.get('/most-selling', getMostSellingProduct);
 productsRouter.get('/low-quantity', getLowQuantityProducts);
 productsRouter.put('/deactivate-product/:productId', deactivateProduct);
+productsRouter.put('/activate-product/:productId', activateProduct);
 
 module.exports = { productsRouter };
